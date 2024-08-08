936c172877281e0eb2a0b0bf052ab8c316dbb46d
Nicolas Cipriano Silva de Oliveira <126813250+cocaow@users.noreply.github.com>
2024-08-08 15:48:20 -0300

    README.py

---
+++ README.py
@@ -0,0 +1,26 @@
+import Trainee from "NicolasCipriano";
+
+class AboutMe extends Trainee {
+  nome = "Nicolas Cipriano Silva de Oliveira";
+  search = "I am looking for an internship opportunity to develop myself";
+  semester = 5;
+  faculdade = "Universidade Federal de Pelotas";
+  local = "Pelotas, RS, Brasil";
+}
+class Skills extends Trainee {
+  knowledge = ["Python", 
+               "C", 
+               "English"];
+  Studing = ["SQL", 
+             "Java"];
+  softskills = ["proactivity",
+               "resilience",
+               "team work",
+               "communicative", 
+               "adaptability"];
+}
+class Contact extends Trainee {
+  Number_WhatsApp = "+5512983033976";
+  LinkedIn = "http://www.linkedin.com/in/nicolascipriano68";
+  Email = "ncsdoliveira@inf.ufpel.edu.br";
+}
